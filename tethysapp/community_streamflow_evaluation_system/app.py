2828387c82e13e4972ca59e1017fe2b66f0cc53b
romer8 <gio.busrom@gmail.com>
2024-08-26 16:07:29 -0600

    changes to not rely on aws auth, but only to access  the public bucket

--- tethysapp/community_streamflow_evaluation_system/app.py
+++ tethysapp/community_streamflow_evaluation_system/app.py
@@ -1,5 +1,4 @@
 from tethys_sdk.base import TethysAppBase
-from tethys_sdk.app_settings import CustomSetting, SecretCustomSetting
 
 class CSES(TethysAppBase):
     """
@@ -19,24 +18,3 @@ class CSES(TethysAppBase):
     enable_feedback = False
     feedback_emails = []
     controller_modules = ["controllers", "State_Controller", "Reach_Controller", "HUC_Controller"]
- 
-    def custom_settings(self):
-        """
-        Example custom_settings method.
-        """
-        custom_settings = (
-            CustomSetting(
-                name='Access_key_ID',
-                type=CustomSetting.TYPE_STRING, 
-                description='AWS bucket S3 Access Key',
-                required=False
-            ),
-            SecretCustomSetting(
-                name='Secret_access_key',
-                description='AWS bucket S3 Secret access Key',
-                required=False
-            ),
-
-        )
-
-        return custom_settings
